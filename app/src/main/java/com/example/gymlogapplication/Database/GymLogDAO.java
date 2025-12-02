152bbfa1c1289588012c1b984bef15dffce8d6ba
Quoc Khanh Ho <qho@csumb.edu>
2025-12-02 00:53:51 -0800

    End of video 3: Database finished

---
+++ app/src/main/java/com/example/gymlogapplication/Database/GymLogDAO.java
@@ -0,0 +1,19 @@
+package com.example.gymlogapplication.Database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.gymlogapplication.Database.entities.GymLog;
+
+import java.util.ArrayList;
+
+@Dao
+public interface GymLogDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(GymLog gymlog);
+
+    @Query("Select * from " + GymLogDatabase.GYM_LOG_TABLE)
+    ArrayList<GymLog> getAllRecords();
+}
--- app/src/main/java/com/example/gymlogapplication/MainActivity.java
+++ app/src/main/java/com/example/gymlogapplication/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
 
-    private static final String TAG = "QUOC_GYMLOG";
+    public static final String TAG = "QUOC_GYMLOG";
 
     String mExercise = "";
     double mWeight = 0.0;
